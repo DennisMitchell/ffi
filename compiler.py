57265062949b3616cf48142f69fbbf60b3398841
DennisMitchell <dennis@tryitonline.net>
2018-02-26 15:27:09 -0300

    added compact output method

--- compiler.py
+++ compiler.py
@@ -1,4 +1,5 @@
 from ctypes import cdll
+from genprint import generate
 from parser import parse_code, parse_integer
 from subprocess import call
 from tempfile import NamedTemporaryFile
@@ -12,7 +13,7 @@ def gen_print_state(primes, file, indentation):
 
 	file.write(');\n')
 
-def run(code, input, print_all):
+def run(code, input, print_all = False, gen_print = 'state'):
 	fractions = list(parse_code(code))
 	input = parse_integer(input)
 	primes= sorted(set.union(*map(set, fractions)) | set(input))
@@ -21,8 +22,8 @@ def run(code, input, print_all):
 	c_file = NamedTemporaryFile(mode = 'w+', suffix = '.c')
 	c_file.write('#include <%s.h>\n' * 4 % ('inttypes', 'signal', 'stdlib', 'stdio') + '\n')
 	c_file.write('void quit(int signal)\n{\n')
-	c_file.write('\tfputs("\\nKeyboardInterrupt\\n", stderr);\n\texit(130);\n}\n\n')
-	c_file.write('void run()\n{\n\tsignal(SIGINT, quit);')
+	c_file.write('\tif (signal == 2) fputs("\\nKeyboardInterrupt\\n", stderr);\n\texit(130);')
+	c_file.write('\n}\n\nvoid run()\n{\n\tsignal(SIGINT, quit);\n\n')
 
 	for prime in primes:
 		c_file.write('\tuint64_t s%u = %u;\n' % (prime, input[prime]))
@@ -30,7 +31,7 @@ def run(code, input, print_all):
 	c_file.write('\n\twhile (1)\n\t{\n')
 
 	if print_all:
-		gen_print_state(primes, c_file, 2)
+		generate(gen_print, primes, c_file, 2)
 
 	for fraction in fractions:
 		conditions = (
@@ -50,12 +51,16 @@ def run(code, input, print_all):
 	c_file.write('\t\tbreak;\n\t}\n\n')
 
 	if not print_all:
-		gen_print_state(primes, c_file, 1)
+		generate(gen_print, primes, c_file, 1)
 
 	c_file.write('}\n')
 	c_file.flush()
 	so_file = NamedTemporaryFile(mode = 'rb', suffix = '.so')
-	call(['cc', '-O2', '-shared', '-fPIC', '-o', so_file.name, c_file.name])
+	if call([
+		'cc', '-std=c99', '-Wall', '-Wextra', '-Werror', '-O2', '-shared', '-fPIC',
+		'-o', so_file.name, c_file.name
+	]):
+		exit(1)
 	c_file.close()
 	program = cdll.LoadLibrary(so_file.name)
 	so_file.close()
--- ffi.py
+++ ffi.py
@@ -23,17 +23,31 @@ def ffi(*args):
 	print_when = argparser.add_mutually_exclusive_group()
 
 	print_when.add_argument(
-		'-f', '--final', dest='print_all', action='store_false', default = False,
-		help = 'Print only the final state. This is the default.'
+		'-f', '--final', dest = 'print_all', action = 'store_false', default = False,
+		help = 'Print only the final state. (default)'
 	)
 
 	print_when.add_argument(
-		'-a', '--all', dest='print_all', action='store_true',
+		'-a', '--all', dest = 'print_all', action = 'store_true',
 		help = 'Print all states, including the initial and the final state.'
 	)
 
+	print_how = argparser.add_mutually_exclusive_group()
+
+	print_how.add_argument(
+		'-c', '--compact', dest = 'print_how', default = 'print_compact',
+		action = 'store_const', const = 'print_compact',
+		help = 'Print a compact representation of the state. (default)'
+	)
+
+	print_how.add_argument(
+		'-w', '--whole', dest = 'print_how',
+		action = 'store_const', const = 'print_state', default = 'print_state',
+		help = 'Print the whole state, including exponents less than 2.'
+	)
+
 	args = argparser.parse_args(list(args) or None)
-	run(open(args.file).read(), args.input, args.print_all)
+	run(open(args.file).read(), args.input, args.print_all, args.print_how)
 
 if __name__ == '__main__':
 	ffi()
