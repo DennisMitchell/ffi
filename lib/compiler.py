d724ee8f6b5521a2951981642702ac7515bb24e2
DennisMitchell <dennis@tryitonline.net>
2018-02-28 13:13:31 -0300

    use subprocess.call instead of cdll.LoadLibrary

--- lib/compiler.py
+++ lib/compiler.py
@@ -1,6 +1,8 @@
-from ctypes import cdll
+from os import chdir
+from shutil import rmtree
 from subprocess import call
-from tempfile import NamedTemporaryFile
+from sys import stderr
+from tempfile import mkdtemp
 from .generate import generate
 from .parser import parse_code, parse_integer
 
@@ -10,29 +12,25 @@ def run(code, input, print_when, print_what, **kwargs):
 	primes= sorted(set.union(*map(set, fractions)) | set(input))
 	numargs = len(primes)
 
-	c_file = NamedTemporaryFile(mode = 'w+', suffix = '.c')
-	c_file.write('#include <%s.h>\n' * 4 % ('inttypes', 'signal', 'stdlib', 'stdio') + '\n')
-	c_file.write('void quit(int signal)\n{\n')
-	c_file.write('\tif (signal == 2) fputs("\\n", stderr);\n\texit(128 | signal);')
-	c_file.write('\n}\n\nvoid run()\n{\n')
+	c_code = []
+	c_code.append('#include <%s.h>\n' * 2 % ('inttypes', 'stdio'))
+	c_code.append('\nint main()\n{\n')
 
 	for prime in primes:
-		c_file.write('\tuint64_t s%u = %u;\n' % (prime, input[prime]))
+		c_code.append('\tuint64_t s%u = %u;\n' % (prime, input[prime]))
 
-	c_file.write('\n\tuint64_t one, quot;\n\n')
-	c_file.write('\tsignal(SIGINT, quit);\n')
-	c_file.write('\tsignal(SIGPIPE, quit);\n')
-	c_file.write('\n\twhile (1)\n\t{\n')
+	c_code.append('\n\tuint64_t one, quot;\n\n')
+	c_code.append('\twhile (1)\n\t{\n')
 
 	if print_when == 'print_all':
-		generate(print_what, primes, c_file, 2, **kwargs)
-		c_file.write('\t\tfflush(stdout);\n\n')
+		generate(print_what, primes, c_code.append, 2, **kwargs)
+		c_code.append('\t\tfflush(stdout);\n\n')
 
 	if print_when == 'print_pow':
-		generate(print_when, primes, c_file, 2, **kwargs)
-		c_file.write('\t\t{\n')
-		generate(print_what, primes, c_file, 3, **kwargs)
-		c_file.write('\t\t\tfflush(stdout);\n\t\t}\n\n')
+		generate(print_when, primes, c_code.append, 2, **kwargs)
+		c_code.append('\t\t{\n')
+		generate(print_what, primes, c_code.append, 3, **kwargs)
+		c_code.append('\t\t\tfflush(stdout);\n\t\t}\n\n')
 
 	for fraction in fractions:
 		conditions = (
@@ -41,31 +39,34 @@ def run(code, input, print_when, print_what, **kwargs):
 			if exponent < 0
 		)
 
-		c_file.write('\t\tif\n\t\t(\n\t\t\t')
-		c_file.write('\n\t\t&&\n\t\t\t'.join(conditions) or '1')
-		c_file.write('\n\t\t)\n\t\t{\n')
+		c_code.append('\t\tif\n\t\t(\n\t\t\t')
+		c_code.append('\n\t\t&&\n\t\t\t'.join(conditions) or '1')
+		c_code.append('\n\t\t)\n\t\t{\n')
 
 		for prime, exponent in fraction.items():
-			c_file.write('\t\t\ts%u += %d;\n' % (prime, exponent))
+			c_code.append('\t\t\ts%u += %d;\n' % (prime, exponent))
 
-		c_file.write('\t\t\tcontinue;\n\t\t}\n\n')
+		c_code.append('\t\t\tcontinue;\n\t\t}\n\n')
 
-	c_file.write('\t\tbreak;\n\t}\n\n')
+	c_code.append('\t\tbreak;\n\t}\n\n')
 
 	if print_when == 'print_final':
-		generate(print_what, primes, c_file, 1, **kwargs)
+		generate(print_what, primes, c_code.append, 1, **kwargs)
 
-	c_file.write('}\n')
-	c_file.flush()
-	so_file = NamedTemporaryFile(mode = 'rb', suffix = '.so')
+	c_code.append('\treturn 0;\n}\n')
+	tempdir = mkdtemp()
+	chdir(tempdir)
+	src = open('program.c', mode = 'w')
+	src.write(''.join(c_code))
+	src.close()
 
-	if call([
-		'cc', '-ansi', '-pedantic', '-Wall', '-Wextra', '-Wno-unused-variable', '-Werror',
-		 '-O2', '-shared', '-fPIC', '-o', so_file.name, c_file.name
-	]):
+	try:
+		assert not call(['cc', '-w', '-O2', '-oprogram', 'program.c'])
+		call(['./program'])
+	except AssertionError:
 		exit(1)
-
-	c_file.close()
-	program = cdll.LoadLibrary(so_file.name)
-	so_file.close()
-	program.run()
+	except KeyboardInterrupt:
+		stderr.write('\n')
+		exit(130)
+	finally:
+		rmtree(tempdir)
--- lib/generate.py
+++ lib/generate.py
@@ -8,19 +8,19 @@ def generate(name, *args, **kwargs):
 		stderr.write('Error: %r is not a valid output method.\n' % name)
 		exit(1)
 
-def gen_print_compact(primes, file, indent, **kwargs):
+def gen_print_compact(primes, emit, indent, **kwargs):
 	tabs = '\t' * indent
-	file.write(tabs + 'one = 1;\n')
+	emit(tabs + 'one = 1;\n')
 
 	for prime in primes:
-		file.write('%sif (s%u == 1) one = ' % (tabs, prime))
-		file.write('!printf("%%s%u", one ? "" : " * ");\n' %  prime)
-		file.write('%selse if (s%u) one = ' % (tabs, prime))
-		file.write('!printf("%%s%u^%%" PRIu64, one ? "" : " * ", s%u);\n' % (prime, prime))
+		emit('%sif (s%u == 1) one = ' % (tabs, prime))
+		emit('!printf("%%s%u", one ? "" : " * ");\n' %  prime)
+		emit('%selse if (s%u) one = ' % (tabs, prime))
+		emit('!printf("%%s%u^%%" PRIu64, one ? "" : " * ", s%u);\n' % (prime, prime))
 
-	file.write(tabs + 'printf("%s\\n", one ? "1" : "");\n')
+	emit(tabs + 'printf("%s\\n", one ? "1" : "");\n')
 
-def gen_print_exp(primes, file, indent, **kwargs):
+def gen_print_exp(primes, emit, indent, **kwargs):
 	try:
 		exps = kwargs['print_exp']
 		exps = list(map(int, exps.rstrip(',').split(',')))
@@ -38,24 +38,24 @@ def gen_print_exp(primes, file, indent, **kwargs):
 
 	for exp in exps:
 		set_quot = tabs + 'quot = %s;\n'
-		file.write(set_quot % 'UINT64_MAX')
+		emit(set_quot % 'UINT64_MAX')
 		exp_factors = factors(exp)
 
 		for prime, exponent in exp_factors.items():
 			if prime not in primes:
-				file.write(set_quot % '__UINT64_C(0)')
+				emit(set_quot % '__UINT64_C(0)')
 			else:
-				file.write(
+				emit(
 					set_quot % 's%u / %u < quot ? s%u / %u : quot'
 					% (prime, exponent, prime, exponent)
 				)
 
-		file.write('%sprintf("%s%%" PRIu64, quot);\n' % (tabs, sep))
+		emit('%sprintf("%s%%" PRIu64, quot);\n' % (tabs, sep))
 		sep = ' '
 
-	file.write(tabs + 'printf("\\n");\n')
+	emit(tabs + 'printf("\\n");\n')
 
-def gen_print_pow(primes, file, indent, **kwargs):
+def gen_print_pow(primes, emit, indent, **kwargs):
 	try:
 		base = kwargs['print_pow']
 		base = int(base)
@@ -68,25 +68,25 @@ def gen_print_pow(primes, file, indent, **kwargs):
 	tabs = '\t' * indent
 
 	if not base:
-		file.write(tabs + 'if(1)\n')
+		emit(tabs + 'if(1)\n')
 		return
 
 	for prime in base:
 		if prime not in primes:
-			file.write(tabs + 'if(0)\n')
+			emit(tabs + 'if(0)\n')
 			return
 
-	file.write(tabs + 'quot = s%u / %u;\n\n' % next(iter(base.items())))
-	file.write('%sif\n%s(\n%s\t' % (tabs, tabs, tabs))
+	emit(tabs + 'quot = s%u / %u;\n\n' % next(iter(base.items())))
+	emit('%sif\n%s(\n%s\t' % (tabs, tabs, tabs))
 	and_ = '\n%s&&\n%s\t' % (tabs, tabs)
-	file.write(and_.join('%u * quot == s%u' % (base[prime], prime) for prime in primes))
-	file.write('\n%s)\n' % tabs)
+	emit(and_.join('%u * quot == s%u' % (base[prime], prime) for prime in primes))
+	emit('\n%s)\n' % tabs)
 
-def gen_print_whole(primes, file, indent, **kwargs):
-	file.write('\t' * indent)
-	file.write('printf("%s\\n"' % ' * '.join('%u^%%" PRIu64 "' % prime for prime in primes))
+def gen_print_whole(primes, emit, indent, **kwargs):
+	emit('\t' * indent)
+	emit('printf("%s\\n"' % ' * '.join('%u^%%" PRIu64 "' % prime for prime in primes))
 
 	for prime in primes:
-		file.write(', s%u' % prime)
+		emit(', s%u' % prime)
 
-	file.write(');\n')
+	emit(');\n')
