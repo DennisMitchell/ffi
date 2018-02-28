d439192eddceb629e61d5de2e7cf6977b684e084
DennisMitchell <dennis@tryitonline.net>
2018-02-28 11:36:56 -0300

    write generated C file all at once

--- lib/compiler.py
+++ lib/compiler.py
@@ -11,25 +11,25 @@ def run(code, input, print_when, print_what, **kwargs):
 	primes= sorted(set.union(*map(set, fractions)) | set(input))
 	numargs = len(primes)
 
-	c_file = NamedTemporaryFile(mode = 'w+', suffix = '.c', delete = False)
-	c_file.write('#include <%s.h>\n' * 2 % ('inttypes', 'stdio'))
-	c_file.write('\nint main()\n{\n')
+	c_code = []
+	c_code.append('#include <%s.h>\n' * 2 % ('inttypes', 'stdio'))
+	c_code.append('\nint main()\n{\n')
 
 	for prime in primes:
-		c_file.write('\tuint64_t s%u = %u;\n' % (prime, input[prime]))
+		c_code.append('\tuint64_t s%u = %u;\n' % (prime, input[prime]))
 
-	c_file.write('\n\tuint64_t one, quot;\n\n')
-	c_file.write('\twhile (1)\n\t{\n')
+	c_code.append('\n\tuint64_t one, quot;\n\n')
+	c_code.append('\twhile (1)\n\t{\n')
 
 	if print_when == 'print_all':
-		generate(print_what, primes, c_file, 2, **kwargs)
-		c_file.write('\t\tfflush(stdout);\n\n')
+		generate(print_what, primes, c_code.append, 2, **kwargs)
+		c_code.append('\t\tfflush(stdout);\n\n')
 
 	if print_when == 'print_pow':
-		generate(print_when, primes, c_file, 2, **kwargs)
-		c_file.write('\t\t{\n')
-		generate(print_what, primes, c_file, 3, **kwargs)
-		c_file.write('\t\t\tfflush(stdout);\n\t\t}\n\n')
+		generate(print_when, primes, c_code.append, 2, **kwargs)
+		c_code.append('\t\t{\n')
+		generate(print_what, primes, c_code.append, 3, **kwargs)
+		c_code.append('\t\t\tfflush(stdout);\n\t\t}\n\n')
 
 	for fraction in fractions:
 		conditions = (
@@ -38,21 +38,23 @@ def run(code, input, print_when, print_what, **kwargs):
 			if exponent < 0
 		)
 
-		c_file.write('\t\tif\n\t\t(\n\t\t\t')
-		c_file.write('\n\t\t&&\n\t\t\t'.join(conditions) or '1')
-		c_file.write('\n\t\t)\n\t\t{\n')
+		c_code.append('\t\tif\n\t\t(\n\t\t\t')
+		c_code.append('\n\t\t&&\n\t\t\t'.join(conditions) or '1')
+		c_code.append('\n\t\t)\n\t\t{\n')
 
 		for prime, exponent in fraction.items():
-			c_file.write('\t\t\ts%u += %d;\n' % (prime, exponent))
+			c_code.append('\t\t\ts%u += %d;\n' % (prime, exponent))
 
-		c_file.write('\t\t\tcontinue;\n\t\t}\n\n')
+		c_code.append('\t\t\tcontinue;\n\t\t}\n\n')
 
-	c_file.write('\t\tbreak;\n\t}\n\n')
+	c_code.append('\t\tbreak;\n\t}\n\n')
 
 	if print_when == 'print_final':
-		generate(print_what, primes, c_file, 1, **kwargs)
+		generate(print_what, primes, c_code.append, 1, **kwargs)
 
-	c_file.write('\treturn 0;\n}\n')
+	c_code.append('\treturn 0;\n}\n')
+	c_file = NamedTemporaryFile(mode = 'w+', suffix = '.c', delete = False)
+	c_file.write(''.join(c_code))
 	c_file.close()
 	bin_file = NamedTemporaryFile(mode = 'rb', delete = False)
 	bin_file.close()
@@ -61,6 +63,7 @@ def run(code, input, print_when, print_what, **kwargs):
 		'cc', '-Wall', '-Wextra', '-Wno-unused-variable', '-Werror',
 		'-O2', '-o', bin_file.name, c_file.name
 	]):
+		remove(c_file.name)
 		exit(1)
 
 	remove(c_file.name)
